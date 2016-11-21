2f32f3f53d3ab54040acc2cfce8e625a37c4cea2
Amali Lakshika <maamalilakshika@gmail.com>
2016-11-21 12:01:32 +0530

    final #1

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
@@ -23,12 +23,34 @@ public class PersistentExpenseManager extends ExpenseManager{
 
     @Override
     public void setup(){
-//        DBHandler dbHandler= new DBHandler(context);
 
-        TransactionDAO persistentTransactionDAO = new PersistentTransactionDAO(context);
-        setTransactionsDAO(persistentTransactionDAO);
+        SQLiteDatabase mydatabase = context.openOrCreateDatabase("140340E", context.MODE_PRIVATE, null);
 
-        AccountDAO persistentAccountDAO = new PersistentAccountDAO(context);
-        setAccountsDAO(persistentAccountDAO);
+        mydatabase.execSQL("CREATE TABLE IF NOT EXISTS account(" +
+                "account_no VARCHAR PRIMARY KEY," +
+                "bank VARCHAR," +
+                "acc_holder VARCHAR," +
+                "balance REAL" +
+                " );");
+
+
+        mydatabase.execSQL("CREATE TABLE IF NOT EXISTS transactionLogger(" +
+                "Transaction_id INTEGER PRIMARY KEY," +
+                "account_no VARCHAR," +
+                "expense_type INT," +
+                "amount REAL," +
+                "transaction_date DATE," +
+                "FOREIGN KEY (account_no) REFERENCES account(account_no)" +
+                ");");
+
+
+
+        //These two functions will hold our DAO instances in memory till the program exists
+        PersistentAccountDAO accountDAO = new PersistentAccountDAO(mydatabase);
+        //accountDAO.addAccount(new Account("Account12","Sampath bank","Manujith",500));
+
+        setAccountsDAO(accountDAO);
+
+        setTransactionsDAO(new PersistentTransactionDAO(mydatabase));
     }
 }
